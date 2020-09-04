d27d5ae25089741bf34fa660af9251d2ce152824
rohanrkamath <rohan22am@gmail.com>
2020-09-04 20:39:31 +0530

    Third commit

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const router = express.Router()
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 // @route GET api/profile
 // @desc Test route
-// @access Public
+// @access Private
 
-router.get('/', (req, res) => res.send('Profile route')); 
+router.get('/me', auth, async (req, res) => {
+    try{
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avatar']);
+
+        if(!profile) {
+            return res.status(400).json({ msg: 'There is no profile for this user.'});
+        }
+
+        res.json(profile);
+
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route POST api/profile
+// @desc Create or Update user profile
+// @access Private
 
 module.exports = router;
